268fb76e1f496a620b59f944b21b7f8d83f27651
Brian Christensen <Brian.ray.christensen@gmail.com>
2020-10-23 10:21:34 -0600

    updates ward routes

--- routes/ward.js
+++ routes/ward.js
@@ -1,10 +1,11 @@
 const knex = require("../knex/knex.js");
-
 var express = require("express");
 var router = express.Router();
 var loggedIn = require("./middleware/auth.middleware").loggedIn;
 var loggedInAdmin = require("./middleware/auth.middleware").loggedInAdmin;
 var randomstring = require("randomstring");
+const user = require("../dao/user.js");
+const ward = require("../dao/ward.js");
 
 const ADMIN = "admin";
 const MEMBER = "member";
@@ -64,9 +65,10 @@ router.get("/program*", (req, res) => {
   }
 });
 
-// TODO test
 router.post("/program*", loggedInAdmin, (req, res) => {
   if (req.body.programItems) {
+    //remove old program items and add new ones
+
     knex("ward")
       .where({ id: req.wardId })
       .update({ program_json: JSON.stringify(req.body) })
@@ -78,91 +80,33 @@ router.post("/program*", loggedInAdmin, (req, res) => {
   }
 });
 
-router.post("/create", loggedIn, (req, res) => {
+router.post("/create", loggedIn, async (req, res) => {
   if (req.body.wardName != null) {
-    knex
-      .from("user_in_ward")
-      .where({ user_id: req.userId, permission_level: ADMIN })
-      .then((rows) => {
-        var isAdmin = rows.length > 0;
-        if (!isAdmin) {
-          // ______start beta check _______
-          knex.from("ward").then((rows) => {
-            if (rows.length >= 10) {
-              //store it in the beta requests table
-              knex
-                .from("user")
-                .where({ id: req.userId })
-                .then((rows) => {
-                  knex("beta_ward_requests")
-                    .insert({
-                      email: rows[0].email,
-                      ward_name: req.body.wardName,
-                    })
-                    .then(() => {
-                      res.status(409).send();
-                    });
-                });
-            } else {
-              // _______end beta check ________
-              knex("ward")
-                .insert({
-                  name: req.body.wardName,
-                  program_json: "{}",
-                  ward_code: createWardCode(),
-                })
-                .then((rows) => {
-                  let wardId = rows[0];
-                  knex("user_in_ward")
-                    .insert({
-                      user_id: req.userId,
-                      ward_id: wardId,
-                      permission_level: ADMIN,
-                    })
-                    .then(() => {
-                      res.send(200);
-                    });
-                });
-              //__start beta check close__
-            }
-          });
-          // __end beta check close__
-        } else {
-          res.send(403);
-        }
-      });
+    //check to make sure they aren't already an admin
+    if(!(await user.getAdminUsersWard(req.userId))){
+      //create a ward and add them as the admin of that ward
+      let wardId = await ward.create(req.body.wardName, createWardCode());
+      await user.makeUserAdmin(req.userId, wardId)
+      res.send(200);
+    } else {
+      res.send(403)
+    }
   } else {
     res.send(400);
   }
 });
 
 router.post("/get_admin_ward_code", loggedInAdmin, (req, res) => {
-  knex
-    .from("ward")
-    .where({ id: req.wardId })
-    .then((rows) => {
-      res.send({ wardCode: rows[0].ward_code });
-    });
+  res.send(ward.getWardCode(req.wardId));
 });
-
-module.exports = router;
 
 function createWardCode() {
   var newCode = randomstring.generate({
     length: 6,
     charset: "BCDFGJKLMNPQRSTVWXYZ",
   });
-
-  // Checks for duplicate ward codes
-  
-  knex
-      .from("ward")
-      .where("ward_code", newCode)
-      .then((rows) => {
-        if (rows.length != 0) {
-          createWardCode();
-        }
-      });
   
   return newCode;
 }
+
+module.exports = router;
