09539b4735b5debf1c729ac0de09214173cc0a4a
Brian Christensen <Brian.ray.christensen@gmail.com>
2020-10-23 10:21:18 -0600

    adds auth token dao methods

--- dao/authToken.js
+++ dao/authToken.js
@@ -1,13 +1,13 @@
 const uuid = require("uuid").v4;
 var connection = require("./connection.js").getConnection;
 
-//untested
+//untested and unused
 let exists = async (token) => {
   return new Promise((resolve, reject) => {
     let query = `SELECT COUNT(AuthTokenID) as count FROM AuthToken WHERE AuthToken=?`;
     let args = [token];
     connection().query(query, args, (error, results, fields) => {
-      if (error) throw error;
+      if (error) reject(error);
       if (results.length > 0 && results[0].count) {
         resolve(true);
       } else {
@@ -31,7 +31,23 @@ let create = async (userId) => {
   });
 };
 
+//untested
+let getUserIDFromToken = async (token) => {
+  return new Promise((resolve, reject) => {
+    let query = `SELECT UserID FROM AuthToken WHERE AuthToken=?`;
+    let args = [token];
+    connection().query(query, args, (error, results, fields) => {
+      if (error) reject(error);
+      if (results && results.length > 0) {
+        resolve(results[0].UserID);
+      }
+      resolve(false);
+    });
+  });
+}
+
 module.exports = {
   create: create,
   exists: exists,
+  getUserIDFromToken: getUserIDFromToken,
 };
