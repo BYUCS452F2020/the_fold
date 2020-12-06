69a2cd5342a118fc0ba806842a95dbc45754099f
Brian Christensen <Brian.ray.christensen@gmail.com>
2020-12-05 18:18:55 -0800

    adds authToken and GoogleLogin Dao's

--- dao/authToken.js
+++ dao/authToken.js
@@ -1,49 +1,54 @@
 const uuid = require("uuid").v4;
 var connection = require("./connection.js").getConnection;
+const AuthToken = require("../mongodb_connections.js").AuthToken();
 
 //untested and unused
 let exists = async (token) => {
-  return new Promise((resolve, reject) => {
-    let query = `SELECT COUNT(AuthTokenID) as count FROM AuthToken WHERE AuthToken=?`;
-    let args = [token];
-    connection().query(query, args, (error, results, fields) => {
-      if (error) reject(error);
-      if (results.length > 0 && results[0].count) {
-        resolve(true);
-      } else {
-        resolve(false);
-      }
-    });
-  });
+  // return new Promise((resolve, reject) => {
+  //   let query = `SELECT COUNT(AuthTokenID) as count FROM AuthToken WHERE AuthToken=?`;
+  //   let args = [token];
+  //   connection().query(query, args, (error, results, fields) => {
+  //     if (error) reject(error);
+  //     if (results.length > 0 && results[0].count) {
+  //       resolve(true);
+  //     } else {
+  //       resolve(false);
+  //     }
+  //   });
+  // });
 };
 
 let create = async (userId) => {
-  return new Promise((resolve, reject) => {
-    let token = uuid();
-    let query = `INSERT INTO AuthToken (UserID, AuthToken) values(?,?);`;
-    let args = [userId, token];
-    connection().query(query, args, (error, results, fields) => {
-      if (error) reject(error);
-      else {
-        resolve(token);
-      }
-    });
-  });
+  let token = uuid();
+  let saveObj = new AuthToken({AuthToken: token, UserID: userId});
+  let doc = await saveObj.save();
+  return doc.AuthToken
+  // return new Promise((resolve, reject) => {
+  //   let token = uuid();
+  //   let query = `INSERT INTO AuthToken (UserID, AuthToken) values(?,?);`;
+  //   let args = [userId, token];
+  //   connection().query(query, args, (error, results, fields) => {
+  //     if (error) reject(error);
+  //     else {
+  //       resolve(token);
+  //     }
+  //   });
+  // });
 };
 
 //untested
 let getUserIDFromToken = async (token) => {
-  return new Promise((resolve, reject) => {
-    let query = `SELECT UserID FROM AuthToken WHERE AuthToken=?`;
-    let args = [token];
-    connection().query(query, args, (error, results, fields) => {
-      if (error) reject(error);
-      if (results && results.length > 0) {
-        resolve(results[0].UserID);
-      }
-      resolve(false);
-    });
-  });
+  // return new Promise((resolve, reject) => {
+  //   let query = `SELECT UserID FROM AuthToken WHERE AuthToken=?`;
+  //   let args = [token];
+  //   connection().query(query, args, (error, results, fields) => {
+  //     if (error) reject(error);
+  //     if (results && results.length > 0) {
+  //       resolve(results[0].UserID);
+  //     }
+  //     resolve(false);
+  //   });
+  // });
 }
 
 module.exports = {
--- dao/googleLogin.js
+++ dao/googleLogin.js
@@ -1,30 +1,39 @@
 var connection = require("./connection.js").getConnection;
+var GoogleLogin = require("../mongodb_connections").GoogleLogin();
 
 let getUserIdFromToken = async (googleToken) => {
-  return new Promise((resolve, reject) => {
-    let query = `SELECT UserID FROM GoogleLogin WHERE GoogleToken=?`;
-    let args = [googleToken];
-    connection().query(query, args, (error, results, fields) => {
-      if (error) throw error;
-      if (results.length > 0) {
-        resolve(results[0].UserID);
-      }
-      resolve();
-    });
-  });
+  let result = await GoogleLogin.find({GoogleToken: googleToken})
+  if(result[0].UserID){
+    return result.UserID
+  }
+  return null;
+  // return new Promise((resolve, reject) => {
+  //   let query = `SELECT UserID FROM GoogleLogin WHERE GoogleToken=?`;
+  //   let args = [googleToken];
+  //   connection().query(query, args, (error, results, fields) => {
+  //     if (error) throw error;
+  //     if (results.length > 0) {
+  //       resolve(results[0].UserID);
+  //     }
+  //     resolve();
+  //   });
+  // });
 };
 
 let create = async (googleID, userId) => {
-  return new Promise((resolve, reject) => {
-    let query = `INSERT INTO GoogleLogin (GoogleToken, UserID) values(?,?);`;
-    let args = [googleID, userId];
-    connection().query(query, args, (error, results, fields) => {
-      if (error) reject(error);
-      else {
-        resolve(results.insertId);
-      }
-    });
-  });
+  let saveObj = new GoogleLogin({GoogleID: googleID, UserID: userId});
+  let result = await saveObj.save();
+
+  // return new Promise((resolve, reject) => {
+  //   let query = `INSERT INTO GoogleLogin (GoogleToken, UserID) values(?,?);`;
+  //   let args = [googleID, userId];
+  //   connection().query(query, args, (error, results, fields) => {
+  //     if (error) reject(error);
+  //     else {
+  //       resolve(results.insertId);
+  //     }
+  //   });
+  // });
 };
 
 module.exports = {
