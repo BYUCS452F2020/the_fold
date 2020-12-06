621729629f1ff8db2537e4b5c8d08f934936e599
Brian Christensen <Brian.ray.christensen@gmail.com>
2020-12-05 18:03:07 -0800

    adds userDao

--- dao/user.js
+++ dao/user.js
@@ -1,45 +1,54 @@
 var connection = require("./connection.js").getConnection;
+const User = require("../mongodb_connections.js").User();
 
 let exists = async (email) => {
-  return new Promise((resolve, reject) => {
-    let query = `SELECT COUNT(UserID) as count FROM User WHERE Email=?`;
-    let args = [email];
-    connection().query(query, args, (error, results, fields) => {
-      if (error) throw error;
-      let temp = results[0];
-      if (results.length > 0 && results[0].count > 0) {
-        resolve(true);
-      } else {
-        resolve(false);
-      }
-    });
-  });
+  let result = await User.find({email: email});
+  if(result.length){
+    return true;
+  }
+  return false;
+  // return new Promise((resolve, reject) => {
+  //   let query = `SELECT COUNT(UserID) as count FROM User WHERE Email=?`;
+  //   let args = [email];
+  //   connection().query(query, args, (error, results, fields) => {
+  //     if (error) throw error;
+  //     let temp = results[0];
+  //     if (results.length > 0 && results[0].count > 0) {
+  //       resolve(true);
+  //     } else {
+  //       resolve(false);
+  //     }
+  //   });
+  // });
 };
 
 let create = async (name, email) => {
-  return new Promise((resolve, reject) => {
-    let query = `INSERT INTO User (FullName, Email) values(?,?);`;
-    let args = [name, email];
-    connection().query(query, args, (error, results, fields) => {
-      if (error) reject(error);
-      resolve(results.insertId);
-    });
-  });
+  let saveObj = new User({FullName: name, Email: email});
+  let result = await saveObj.save();
+  return result._id;
+  // return new Promise((resolve, reject) => {
+  //   let query = `INSERT INTO User (FullName, Email) values(?,?);`;
+  //   let args = [name, email];
+  //   connection().query(query, args, (error, results, fields) => {
+  //     if (error) reject(error);
+  //     resolve(results.insertId);
+  //   });
+  // });
 };
 
 //untested
 let getAdminUsersWard = (userId) => {
-  return new Promise((resolve, reject) => {
-    let query = `SELECT * FROM WardAdmin WHERE UserID=?`;
-    let args = [userId];
-    connection().query(query, args, (error, results, fields) => {
-      if (error) reject(error);
-      if(results.length > 0){
-        resolve(results[0].WardID);
-      }
-      resolve(false);
-    })
-  })
+  // return new Promise((resolve, reject) => {
+  //   let query = `SELECT * FROM WardAdmin WHERE UserID=?`;
+  //   let args = [userId];
+  //   connection().query(query, args, (error, results, fields) => {
+  //     if (error) reject(error);
+  //     if(results.length > 0){
+  //       resolve(results[0].WardID);
+  //     }
+  //     resolve(false);
+  //   })
+  // })
 }
 
 
