b9159f871b76768489e94c3d74ea4f58e27ae784
Michael Lavin <43680530+MichaelLavin778@users.noreply.github.com>
2020-12-05 18:21:09 -0700

    Finished MongoDB Table creators
    create all other tables in mongodb

--- mongodb_connections.js
+++ mongodb_connections.js
@@ -9,29 +9,66 @@ function startMongoDB() {
 }
 
 function User() {
-    // Create a scheme for items in the museum: a title and a path to an image.
+    // Create a scheme.
     const userSchema = new mongoose.Schema({
         UserID: Number,
         FullName: String,
         Email: String,
     });
     
-    // Create a model for items in the museum.
+    // Create a model.
     const user = mongoose.model('User', userSchema);
     return user;
 }
 
 function AuthToken() {
-
+    // Create a scheme.
+    const authTokenSchema = new mongoose.Schema({
+        AuthTokenID: Number,
+        AuthToken: String,
+        UserID: Number
+    });
+    
+    // Create a model.
+    const authToken = mongoose.model('authToken', authTokenSchema);
+    return authToken;
 }
 function GoogleLogin() {
-
+    // Create a scheme.
+    const googleLoginSchema = new mongoose.Schema({
+        GoogleLoginID: Number,
+        GoogleToken: String,
+        UserID: Number
+    });
+    
+    // Create a model.
+    const googleLogin = mongoose.model('GoogleLogin', googleLoginSchema);
+    return googleLogin;
 }
 function Ward() {
-
+    // Create a scheme.
+    const wardSchema = new mongoose.Schema({
+        WardID: Number,
+        Code: String,
+        Name: String,
+        ProgramItem: Array
+    });
+    
+    // Create a model.
+    const wardLogin = mongoose.model('Ward', wardSchema);
+    return wardLogin;
 }
 function WardAdmin() {
-
+    // Create a scheme.
+    const wardAdminSchema = new mongoose.Schema({
+        WardAdminID: Number,
+        UserID: Number,
+        WardID: Number,
+    });
+    
+    // Create a model.
+    const wardAdmin = mongoose.model('WardAdmin', wardAdminSchema);
+    return wardAdmin;
 }
 
 module.exports = {
