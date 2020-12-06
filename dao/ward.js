d9c69fb2283d8f097a8dd3ad1e78a57c17597f96
Michael Lavin <43680530+MichaelLavin778@users.noreply.github.com>
2020-12-05 19:17:26 -0700

    Changed ward dao to mongodb
    ^

--- dao/ward.js
+++ dao/ward.js
@@ -1,107 +1,140 @@
 var connection = require("./connection.js").getConnection;
+const Ward = require('./../mongodb_connections.js').Ward;
+const WardAdmin = require('./../mongodb_connections.js').WardAdmin;
 
-//untested
+
 let create = async (name, code) => {
-  return new Promise((resolve, reject) => {
-    let query = `INSERT INTO Ward (Name, Code) values(?, ?);`;
-    let args = [name, code];
-    connection().query(query, args, (error, results, fields) => {
-      if (error) reject(error);
-      resolve(results.insertId);
-    });
-  });
+    let saveObj = new Ward({ Name: name, Code: code });
+    let result = await saveObj.save();
+    return result._id;
+//   return new Promise((resolve, reject) => {
+//     let query = `INSERT INTO Ward (Name, Code) values(?, ?);`;
+//     let args = [name, code];
+//     connection().query(query, args, (error, results, fields) => {
+//       if (error) reject(error);
+//       resolve(results.insertId);
+//     });
+//   });
 };
 
 let getWardIDFromCode = (wardCode) => {
-    return new Promise((resolve, reject) => {
-        let query = `SELECT WardID FROM Ward WHERE Code=?`;
-        let args = [wardCode];
-        connection().query(query, args, (error, results) => {
-            if (error) reject(error);
-            resolve(results[0].WardID);
-        })
-    })
+    let result = await Ward.find({Code: wardCode});
+    if (result.length) {
+        return result[0]._id;
+    }
+    return null;
+    // return new Promise((resolve, reject) => {
+    //     let query = `SELECT WardID FROM Ward WHERE Code=?`;
+    //     let args = [wardCode];
+    //     connection().query(query, args, (error, results) => {
+    //         if (error) reject(error);
+    //         resolve(results[0].WardID);
+    //     })
+    // })
 }
 
 let getWardName = (wardId) => {
-    return new Promise((resolve, reject) => {
-        let query = `SELECT Name FROM Ward WHERE WardID=?`;
-        let args = [wardId];
-        connection().query(query, args, (error, results) => {
-            if (error) reject(error);
-            resolve(results[0].Name);
-        })
-    })
+    let result = await Ward.find({_id: wardId});
+    if (result.length) {
+        return result[0].Name;
+    }
+    return null;
+    // return new Promise((resolve, reject) => {
+    //     let query = `SELECT Name FROM Ward WHERE WardID=?`;
+    //     let args = [wardId];
+    //     connection().query(query, args, (error, results) => {
+    //         if (error) reject(error);
+    //         resolve(results[0].Name);
+    //     })
+    // })
 }
 
 let getWardCode = (wardId) => {
-    return new Promise((resolve, reject) => {
-        let query = `SELECT Code FROM Ward WHERE WardID=?`;
-        let args = [wardId];
-        connection().query(query, args, (error, results) => {
-            if (error) reject(error);
-            resolve(results[0].Code);
-        })
-    })
+    let result = await Ward.find({_id: wardId});
+    if (result.length) {
+        return result[0].Code;
+    }
+    return null;
+    // return new Promise((resolve, reject) => {
+    //     let query = `SELECT Code FROM Ward WHERE WardID=?`;
+    //     let args = [wardId];
+    //     connection().query(query, args, (error, results) => {
+    //         if (error) reject(error);
+    //         resolve(results[0].Code);
+    //     })
+    // })
 }
 
 let getProgram = (wardId) => {
-    return new Promise((resolve, reject) => {
-        let query = `SELECT Title, SubTitle, Type FROM ProgramItem WHERE WardID=?`;
-        let args = [wardId];
-        connection().query(query, args, (error, results, fields) => {
-            if (error) reject(error);
-            else {
-                resolve(results);
-            }
-        })
-    })
+    let result = await Ward.find({_id: wardId});
+    if (result.length) {
+        return result[0].Program;
+    }
+    return null;
+    // return new Promise((resolve, reject) => {
+    //     let query = `SELECT Title, SubTitle, Type FROM ProgramItem WHERE WardID=?`;
+    //     let args = [wardId];
+    //     connection().query(query, args, (error, results, fields) => {
+    //         if (error) reject(error);
+    //         else {
+    //             resolve(results);
+    //         }
+    //     })
+    // })
 };
 
 let setProgram = (wardId, program) => {
-    return new Promise((resolve, reject) => {
+    await Ward.updateOne({ _id: wardId }, {
+        Program: program
+      });
+    // return new Promise((resolve, reject) => {
 
-        let query = `DELETE FROM ProgramItem WHERE WardID=?`;
-        let args = [wardId]
-        connection().query(query, args, (error, results, fields) => {
-            if (error) reject(error);
-            else {
-                query = '';
-                args = [];
-                // loop to create query and args
-                for(let i = 0; i < program.length; i++){
-                    query += `INSERT INTO ProgramItem (Position, Title, SubTitle, Type, WardID) VALUES(?, ?, ?, ?, ?); `;
-                    args.push(program[i].Position);
-                    args.push(program[i].Title);
-                    args.push(program[i].SubTitle);
-                    args.push(program[i].Type);
-                    args.push(program[i].WardID);
-                }
-                connection().query(query, args, (error, results, fields) => {
-                    if (error) reject(error);
-                    else {
-                        resolve();
-                    }
-                });
-            }
-        })
-    })
+    //     let query = `DELETE FROM ProgramItem WHERE WardID=?`;
+    //     let args = [wardId]
+    //     connection().query(query, args, (error, results, fields) => {
+    //         if (error) reject(error);
+    //         else {
+    //             query = '';
+    //             args = [];
+    //             // loop to create query and args
+    //             for(let i = 0; i < program.length; i++){
+    //                 query += `INSERT INTO ProgramItem (Position, Title, SubTitle, Type, WardID) VALUES(?, ?, ?, ?, ?); `;
+    //                 args.push(program[i].Position);
+    //                 args.push(program[i].Title);
+    //                 args.push(program[i].SubTitle);
+    //                 args.push(program[i].Type);
+    //                 args.push(program[i].WardID);
+    //             }
+    //             connection().query(query, args, (error, results, fields) => {
+    //                 if (error) reject(error);
+    //                 else {
+    //                     resolve();
+    //                 }
+    //             });
+    //         }
+    //     })
+    // })
 };
 
 let isAdmin = (wardId, userId) => {
-    return new Promise((resolve, reject) => {
-        let query = `SELECT * FROM WardAdmin WHERE WardID=? AND UserID=?`;
-        let args = [wardId, userId];
-        connection().query(query, args, (error, results) => {
-            if (error) reject(error);
-            if(results.length === 1){
-                resolve(true);
-            }
-            else {
-                resolve(false);
-            }
-        })
-    })
+    let result = await WardAdmin.find({WardID: wardId, UserID: userId});
+    if (result.length) {
+        return true;
+    }
+    return false;
+    // return new Promise((resolve, reject) => {
+    //     let query = `SELECT * FROM WardAdmin WHERE WardID=? AND UserID=?`;
+    //     let args = [wardId, userId];
+    //     connection().query(query, args, (error, results) => {
+    //         if (error) reject(error);
+    //         if(results.length === 1){
+    //             resolve(true);
+    //         }
+    //         else {
+    //             resolve(false);
+    //         }
+    //     })
+    // })
 }
 
 module.exports = {
